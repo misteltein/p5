128215261b23eba82153d1c31f1820584c440e76
misteltein <itogahibiki@gmail.com>
2021-01-18 20:29:18 +0900

    added

---
+++ p5Tutorial/Ie.js
@@ -0,0 +1,18 @@
+let sketch = function(p){
+   p.setup=function setup() {
+      p.createCanvas(500, 500)
+      p.background(p.color(100, 100, 100))
+      p.circle(0,   p.height / 2, 50)
+      p.circle(50,  p.height / 2, 50)
+      p.circle(100, p.height / 2, 50)
+      p.circle(150, p.height / 2, 50)
+      p.circle(200, p.height / 2, 50)
+      p.circle(250, p.height / 2, 50)
+      p.circle(300, p.height / 2, 50)
+      p.circle(350, p.height / 2, 50)
+      p.circle(400, p.height / 2, 50)
+      p.circle(450, p.height / 2, 50)
+      p.circle(500, p.height / 2, 50)
+   }
+}
+new p5(sketch)
