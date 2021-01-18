01ec4076cd6cf3e6d8fbe675b2a23956380ae88e
misteltein <itogahibiki@gmail.com>
2021-01-18 20:02:37 +0900

    added

---
+++ p5Tutorial/Id.js
@@ -0,0 +1,8 @@
+let sketch=function(p){
+   p.setup = function setup() {
+       p.createCanvas(500, 500)
+       p.background(p.color(100, 100, 100))
+       p.circle(p.width / 2, p.height / 2, p.width)
+   }
+}
+new p5(sketch)
