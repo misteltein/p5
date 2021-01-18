a38de92f1c546ddb0119eb837d285bd45d40de35
misteltein <itogahibiki@gmail.com>
2021-01-18 09:50:32 +0900

    fixed

--- p5Tutorial/Ia.js
+++ p5Tutorial/Ia.js
@@ -2,7 +2,7 @@ let sketch = function(p)
 {
    p.setup = function setup() {
       p.createCanvas(100, 200)
-      p.background(color(100, 100, 100))
+      p.background(p.color(100, 100, 100))
    }
 }
 new p5(sketch)
