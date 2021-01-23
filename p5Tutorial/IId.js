54395691c39420b712561dee069e94fb589529c2
misteltein <itogahibiki@gmail.com>
2021-01-23 15:39:29 +0900

    u

--- p5Tutorial/IId.js
+++ p5Tutorial/IId.js
@@ -4,29 +4,14 @@ let sketch = function(p){
        p.background(p.color(100, 100, 100))
    }
    
-   let x0 = 0
-   let x1 = 0
-   let x2 = 0
-   let x3 = 0
-   let x4 = 0
+   let x=0
    
-   p.draw = function draw() {
-       p.background(p.color(100, 100, 100))
-       p.circle(x0,                 0.3 * p.height, 100)
-       p.circle(x1 + 0.1 * p.width, 0.4 * p.height, 100)
-       p.circle(x2 + 0.2 * p.width, 0.5 * p.height, 100)
-       p.circle(x3 + 0.3 * p.width, 0.6 * p.height, 100)
-       p.circle(x4 + 0.4 * p.width, 0.7 * p.height, 100)
-       x0+=1 
-       x1+=1 
-       x2+=1 
-       x3+=1 
-       x4+=1 
-      if(x0>p.width){x0=0}
-      if(x1>p.width){x1=0}
-      if(x2>p.width){x2=0}
-      if(x3>p.width){x3=0}
-      if(x4>p.width){x4=0}
+   p.draw = function draw(){
+     p.background(p.color(100, 100, 100))
+     for(let i=0;i<5;++i){
+       p.circle(x + 0.1*i*p.width, (0.3+0.1*i)* p.height, 100)
+     }
+     x=x+1
    }
 }
 new p5(sketch)
