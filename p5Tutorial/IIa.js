053f1ae8d9afea058f1d6f69c06eaa25fd52642f
misteltein <itogahibiki@gmail.com>
2021-01-23 14:48:12 +0900

    fixed

--- p5Tutorial/IIa.js
+++ p5Tutorial/IIa.js
@@ -1,8 +1,8 @@
 let sketch = function(p){
    p.setup = function setup() {
-    createCanvas(500, 500)
-    background(color(100, 100, 100))
-    circle(0, 0.5*height, 100)
+    p.createCanvas(500, 500)
+    p.background(color(100, 100, 100))
+    p.circle(0, 0.5*p.height, 100)
    }
 }
 new p5(sketch)
