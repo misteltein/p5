cf96f7d03ab0fc2b554c96cc00a937d32006cdfb
misteltein <itogahibiki@gmail.com>
2021-01-23 15:00:12 +0900

    fixed

--- p5Tutorial/IIb.js
+++ p5Tutorial/IIb.js
@@ -9,7 +9,9 @@ let sketch = function(p){
    p.draw = function draw() {
        p.circle(x, 0.5 * p.height, 100)
        x = x + 1
-       if(x>p.width){x=0}
+       if(x>p.width){x=0
+         p.background(p.color(100, 100, 100))
+       }
    }
 }
 
