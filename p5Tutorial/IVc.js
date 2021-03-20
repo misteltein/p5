1252c3f8948f71f739d6765932a27dcb8ef08359
misteltein <itogahibiki@gmail.com>
2021-03-20 14:56:50 +0900

    added

---
+++ p5Tutorial/IVc.js
@@ -0,0 +1,34 @@
+let sketch = function(p){
+   const W = 300
+   const H = 300
+   
+   let x = W * 0.5
+   let y = H * 0.5
+   
+   p.draw = function(){
+       if (p.keyIsDown(p.LEFT_ARROW)) {
+           x -= 1
+           x = max(0, x)
+       }
+       if (p.keyIsDown(p.RIGHT_ARROW)) {
+           x += 1
+           x = min(x, W)
+       }
+       if (p.keyIsDown(p.UP_ARROW)) {
+           y -= 1
+           y = max(0, y)
+       }
+       if (p.keyIsDown(p.DOWN_ARROW)) {
+           y += 1
+           y = min(y, H)
+       }
+       p.background(200)
+       p.circle(x, y, 100)
+   }
+   
+   p.setup = function(){
+       p.createCanvas(W, H)
+   }
+}
+
+new p5(sketch)
