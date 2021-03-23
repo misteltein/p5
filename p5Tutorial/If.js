8c0f223a49e2536b598cf2f2b67056f022383624
misteltein <itogahibiki@gmail.com>
2021-03-24 02:26:00 +0900

    u

--- p5Tutorial/If.js
+++ p5Tutorial/If.js
@@ -1,12 +1,10 @@
 let sketch=function(p){
    p.setup = function setup() {
-      p.createCanvas(500, 500)
+      p.createCanvas(300, 300)
       p.background(p.color(100, 100, 100))
-      const num = 10
-      const delta = p.width / num
-      for (let i = 0; i <= num; ++i) {
-         for (let j = 0; j <= num; ++j) {
-            p.circle(i * delta, j * delta, delta)
+      for (let i = 0; i < 7; ++i) {
+         for (let j = 0; j < 7; ++j) {
+            p.circle(i * 50, j * 50, 50)
          }
       }
    }
