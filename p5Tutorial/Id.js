ff8e43155fc161407927dc282579d3a6c3a7c68e
misteltein <itogahibiki@gmail.com>
2021-03-24 02:09:02 +0900

    u

--- p5Tutorial/Id.js
+++ p5Tutorial/Id.js
@@ -1,6 +1,6 @@
 let sketch=function(p){
    p.setup = function setup() {
-      p.createCanvas(500, 500)
+      p.createCanvas(300, 300)
       p.background(p.color(100, 100, 100))
       p.circle(p.width / 2, p.height / 2, p.width)
    }
