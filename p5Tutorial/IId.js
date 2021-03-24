88a2dba57ca90ed0fd17df804fad29a854e650cc
misteltein <itogahibiki@gmail.com>
2021-03-24 09:17:19 +0900

    updated

--- p5Tutorial/IId.js
+++ p5Tutorial/IId.js
@@ -1,6 +1,6 @@
 let sketch = function(p){
    p.setup = function setup() {
-       p.createCanvas(500, 500)
+       p.createCanvas(300, 300)
        p.background(p.color(100, 100, 100))
    }
    
@@ -9,7 +9,7 @@ let sketch = function(p){
    p.draw = function draw(){
      p.background(p.color(100, 100, 100))
      for(let i=0;i<5;++i){
-       p.circle(x + 0.1*i*p.width, (0.3+0.1*i)* p.height, 100)
+       p.circle(x + 0.1*i*p.width, (0.3+0.1*i)* p.height, 50)
      }
      x=x+1
      if(x>p.width){
