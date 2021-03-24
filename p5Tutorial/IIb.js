55a29e690d72bb9628f6fecd5fc5476eef75e157
misteltein <itogahibiki@gmail.com>
2021-03-24 09:06:24 +0900

    u

--- p5Tutorial/IIb.js
+++ p5Tutorial/IIb.js
@@ -1,13 +1,13 @@
 let sketch = function(p){
    p.setup = function setup() {
-      p.createCanvas(500, 500)
+      p.createCanvas(300, 300)
       p.background(p.color(100, 100, 100))
    }
 
    let x = 0;
    
    p.draw = function draw() {
-       p.circle(x, 0.5 * p.height, 100)
+       p.circle(x, 0.5 * p.height, 50)
        x = x + 1
        if(x>p.width){x=0
          p.background(p.color(100, 100, 100))
